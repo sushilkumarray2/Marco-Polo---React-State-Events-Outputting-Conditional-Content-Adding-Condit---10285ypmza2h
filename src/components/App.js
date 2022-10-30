88d9e87c24741cf1333fa391baf783067a3f2ff8
sushilkumarray2 <114345825+sushilkumarray2@users.noreply.github.com>
2022-10-31 04:10:03 +0530

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../styles/App.css';
 const App = () => {
-  
+  const [state, setState] = useState(true);
+
+  const handle = ()=>{
+    setState(!state);
+  }
   return (
     <div id="main">
-
+      <h1 id="marco-polo">{state ? "Marco" : "Polo"}</h1>
+      <button id="marco-polo-toggler" onClick={()=>handle()}>{(!state) ? "Marco":"Polo"}</button>
     </div>
   )
 }
